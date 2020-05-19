99a0dce38f8f248729e0c77549a042e1870f862f
fin <qiuxchao@users.noreply.github.com>
2020-05-19 13:09:25 +0800

    's'

---
+++ README.md
@@ -0,0 +1,24 @@
+# chat-app
+
+这是一个基于 Node.js 开发的实时多人在线聊天应用。
+
+![](https://cdn.jsdelivr.net/gh/qiuxchao/CDN/chatApp/1.png)
+
+**在线地址：** http://www.qiuxc.cn:5000
+
+
+**项目简介：** 该项目使用 express 框架搭建 http 服务器，使用 socket.io 模块实现客户端与服务器的实时双向通信、服务器向所有连接的客户端广播数据等功能。最后使用 localStorage 本地存储技术将用户接收到的消息缓存到本地。
+
+**技术栈：** **Node.js + express + socket.io**
+
+
+
+
+
+
+
+
+
+
+
+
--- public/chat.js
+++ public/chat.js
@@ -1,6 +1,9 @@
 // 设置 socket 客户端
 // 实现和服务端的连接
 let socket = io.connect('http://localhost:5000');
+// let socket = io.connect('http://www.qiuxc.cn:5000');
+
+
 
 // 获取 DOM 节点
 let message = document.getElementById('message'),   // 消息输入框
@@ -9,6 +12,41 @@ let message = document.getElementById('message'),   // 消息输入框
     btn = document.getElementById('send'),              // 发送按钮
     output = document.getElementById('output'),     // 消息输出文本
     feedback = document.getElementById('feedback'); // xxx正在发送消息文本
+
+
+// 从本地存储中拿到值，遍历到 DOM 中
+// 模拟值：
+// datas = [
+//     { self: {name: 'lisi', message: 'nihao'} },
+//     { data: { handle: 'zhangsan', message: 'nihao' } }
+// ]
+let datas = JSON.parse(localStorage.getItem('datas'));
+if (datas !== null) {
+    // 本地有值，遍历到 DOM 中
+    for (item of datas) {
+        if ( item.self ) {
+            // 将自己发的消息显示到右侧
+            output.innerHTML += `
+            <p class="self"><span>${item.self.message}</span></p>
+            `;
+            // 将自己的昵称放到昵称输入框中
+            handle.value = item.self.name;
+        }
+        if (item.data) {
+            // 将其他人消息数据更新到 DOM 节点中
+            output.innerHTML += `
+            <p><strong>${ item.data.handle}：</strong><span>${item.data.message}</span></p>
+            `;
+        }
+    }
+    // 滚动到消息区底部
+    chatWindow.scrollBy(0, chatWindow.scrollHeight);
+} else {
+    // 本地没有值，设置一个空数组
+    datas = [];
+}
+
+
 
 // 事件监听
 function send() {
@@ -23,6 +61,10 @@ function send() {
     output.innerHTML += `
     <p class="self"><span>${message.value}</span></p>
     `;
+
+    // 将自己发的消息存储到本地
+    datas.push({ self: {name: handle.value, message: message.value } });
+    localStorage.setItem('datas', JSON.stringify(datas));
 
     // 清空信息框的值
     message.value = '';
@@ -43,10 +85,14 @@ message.addEventListener('keydown', () => {
 socket.on('chat', (data) => {
     // 清空提示信息
     feedback.innerHTML = '';
-    // 将数据更新到 DOM 节点中
+    // 将其他人消息数据更新到 DOM 节点中
     output.innerHTML += `
     <p><strong>${ data.handle}：</strong><span>${data.message}</span></p>
     `;
+
+    // 将其他人的消息存储到本地
+    datas.push({ data: { handle: data.handle, message: data.message } });
+    localStorage.setItem('datas', JSON.stringify(datas));
 
     // 滚动到消息区底部
     chatWindow.scrollBy(0, chatWindow.scrollHeight);
